acf7879061e506930159048fbe4abf992fcaae53
Azathotep <james.sutherland2014@gmail.com>
2014-05-18 20:40:17 +0100

    Add compass methods

--- LiteEngine/Math/Compass.cs
+++ LiteEngine/Math/Compass.cs
@@ -8,19 +8,35 @@ namespace LiteEngine.Math
 {
     public class Compass
     {
-        public static Vector2 GetVector(CardinalDirection direction)
+        public static Vector2 GetVector2(CardinalDirection direction)
         {
             switch (direction)
             {
                 case CardinalDirection.North:
-                    return new Vector2(0, -1f);
+                    return new Vector2(0, -1);
                 case CardinalDirection.East:
                     return new Vector2(1, 0);
                 case CardinalDirection.South:
-                    return new Vector2(0, 1f);
+                    return new Vector2(0, 1);
                 case CardinalDirection.West:
                 default:
-                    return new Vector2(-1f, 0);
+                    return new Vector2(-1, 0);
+            }
+        }
+
+        public static CardinalDirection GetOppositeDirection(CardinalDirection direction)
+        {
+            switch (direction)
+            {
+                case CardinalDirection.North:
+                    return CardinalDirection.South;
+                case CardinalDirection.West:
+                    return CardinalDirection.East;
+                case CardinalDirection.East:
+                    return CardinalDirection.West;
+                case CardinalDirection.South:
+                default:
+                    return CardinalDirection.North;
             }
         }
     }
