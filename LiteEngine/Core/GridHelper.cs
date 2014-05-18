6fd0c95d03e7e2956d8da5fc04e73acadddd1a2b
Azathotep <james.sutherland2014@gmail.com>
2014-05-18 19:33:53 +0100

    Add foreach mechanism for 2D array

---
+++ LiteEngine/Core/GridHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiteEngine.Core
+{
+    public class GridHelper
+    {
+        /// <summary>
+        /// Provides 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="callback"></param>
+        public static void Foreach<T>(T[,] array, Action<int, int> callback)
+        {
+            for (int y = 0; y <= array.GetUpperBound(1); y++)
+                for (int x = 0; x <= array.GetUpperBound(0); x++)
+                    callback(x, y);
+        }
+    }
+}
